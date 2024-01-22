4d261c75a736c2781cc762387b8efa7502bfa706
Munoru101 <dennismunoru@gmail.com>
2024-01-22 19:23:51 +0300

    Updated header file

--- sort.h
+++ sort.h
@@ -19,7 +19,7 @@ struct listint_s *prev;
 struct listint_s *next;
 } listint_t;
 
-void quick_start(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 #endif
