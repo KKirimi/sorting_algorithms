dbf050bccfb8e2ddabfecbf246b79cc06a756c8d
KKirimi <keithkirimi.kk@gmail.com>
2024-01-23 09:28:19 +0000

    0x1B. C - Sorting algorithms & Big O

--- sort.h
+++ sort.h
@@ -1,9 +1,9 @@
-#ifndef SORT_H
-#define SORT_H
+#ifndef _SORT_H
+#define _SORT_H
 
-#include <stddef.h>
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -12,17 +12,33 @@
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
  */
+
 typedef struct listint_s
 {
-const int n;
-struct listint_s *prev;
-struct listint_s *next;
+	int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
-void swap(int *a, int *b);
+
+/* print.c */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-int lomuto_partition(int *array, size_t size, int low, int high);
-void quick_sort_recursively(int *array, size_t size, int low, int high);
+void check_list(const listint_t *list);
+void check_array(const int *array, size_t size);
+int *rand_array(int len, int max);
+
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void merge_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void counting_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+
 #endif
